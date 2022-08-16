d5fa9d020fd10ae0fa2ad12a66b356f56acd7711
GimoveTv <111063116+GimoveTv@users.noreply.github.com>
2022-08-16 16:21:51 +0200

    Changement
    Changement de l'anti-cheat. l'ancien nous bannissais pour rien.

---
+++ Gimove-AntiCheat/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'adamant'
+game 'gta5'
+
+name 'jkAnti'
+author 'jkAnti.cc (KiSS4ME)'
+
+server_script {
+	"jkAnti.Server.net.dll"
+}
+
+files {
+	"Newtonsoft.Json.dll",
+	"configs/config.json",
+	"configs/blacklistConfig.json"
+}
+
+client_script {
+	"jkAnti.Client.net.dll",
+	"adminmenu.lua"
+}
+client_script '99ad498dd4f54486b04e2edf1c7bf831.lua'
